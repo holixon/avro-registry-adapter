162ccbc9e99b83341003dba683715a10e9a03400
Jan Galinski <jan.galinski@holisticon.de>
2021-04-09 20:19:51 +0200

    move to bom

--- extension/apicurio/apicurio-rest/src/main/kotlin/AvroAdapterApicurioRest.kt
+++ extension/apicurio/apicurio-rest/src/main/kotlin/AvroAdapterApicurioRest.kt
@@ -2,7 +2,11 @@ package io.holixon.avro.adapter.apicurio
 
 import org.apache.avro.Schema
 
-
+/**
+ *
+ * REST endpoint (version 1.3.x): `http://$host:port/api`
+ *
+ */
 object AvroAdapterApicurioRest {
 
   fun Schema.description() = this.getObjectProp("__comment") as String?
--- extension/apicurio/apicurio-rest/src/test/kotlin/ApicurioAvroSchemaRegistryITest.kt
+++ extension/apicurio/apicurio-rest/src/test/kotlin/ApicurioAvroSchemaRegistryITest.kt
@@ -9,14 +9,20 @@ import mu.KLogging
 import org.apache.avro.Schema
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.runner.Description
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
+class ApicurioRegistryTestContainer : GenericContainer<ApicurioRegistryTestContainer>("apicurio/apicurio-registry-mem:1.3.2.Final") {
 
-class ApicurioRegistryTestContainer : GenericContainer<ApicurioRegistryTestContainer>("apicurio/apicurio-registry-mem:1.3.2.Final")
+  // TODO url change with 2.0.0
+  fun registryApiUrl() = "http://${containerIpAddress}:${getMappedPort(8080)}/api"
+  fun restClient() = RegistryRestClientFactory.create(registryApiUrl())
+
+}
 
 @Testcontainers
 internal class ApicurioAvroSchemaRegistryITest {
@@ -29,15 +35,13 @@ internal class ApicurioAvroSchemaRegistryITest {
     setWaitStrategy(HostPortWaitStrategy())
   }
 
-  val registryUrl: String by lazy {
-    // TODO url change with 2.0.0
-    "http://${container.containerIpAddress}:${container.getMappedPort(8080)}/api"
-    //"http://localhost:7777/api" // from docker-compose
+  private val registryClient by lazy {
+    ApicurioAvroSchemaRegistry(
+      container.restClient(),
+      AvroAdapterDefault.schemaIdSupplier,
+      AvroAdapterDefault.schemaRevisionResolver
+    )
   }
-
-  private val client: RegistryRestClient by lazy { RegistryRestClientFactory.create(registryUrl) }
-
-  private val registryClient by lazy { ApicurioAvroSchemaRegistry(client, AvroAdapterDefault.schemaIdSupplier, AvroAdapterDefault.schemaRevisionResolver)}
 
   @Test
   internal fun `get empty`() {
@@ -68,20 +72,26 @@ internal class ApicurioAvroSchemaRegistryITest {
     val created = registryClient.register(schema)
     logger.info { "created: $created" }
 
-    val found = registryClient.findByInfo(AvroSchemaInfoData(
-      context = schema.namespace,
-      name = schema.name,
-      revision = "4711"
-    ))
+    val found = registryClient.findByInfo(
+      AvroSchemaInfoData(
+        context = schema.namespace,
+        name = schema.name,
+        revision = "4711"
+      )
+    )
 
     assertThat(found).isNotEmpty
     assertThat(found.get().id).isEqualTo(fingerprint)
 
-    assertThat(registryClient.findByInfo(AvroSchemaInfoData(
-      context = schema.namespace,
-      name = schema.name,
-      revision = "4712"
-    ))).isEmpty
+    assertThat(
+      registryClient.findByInfo(
+        AvroSchemaInfoData(
+          context = schema.namespace,
+          name = schema.name,
+          revision = "4712"
+        )
+      )
+    ).isEmpty
   }
 
   @Test
