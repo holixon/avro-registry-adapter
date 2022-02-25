32fe69abe13b0fb8175c1a2efd7178b46d46c1cd
Jan Galinski <jan.galinski@holisticon.de>
2022-02-25 15:17:24 +0100

    add kdoc

--- extension/default/src/main/kotlin/DefaultAvroSchemaRevisionForSchemaIdResolver.kt
+++ extension/default/src/main/kotlin/DefaultAvroSchemaRevisionForSchemaIdResolver.kt
@@ -3,11 +3,18 @@ package io.holixon.avro.adapter.common
 import io.holixon.avro.adapter.api.*
 import java.util.*
 
+/**
+ * An implmentation of [AvroSchemaRevisionForSchemaIdResolver] that uses an [AvroSchemaResolver] (aka registry) to
+ * load a schema and then forwards the revision resolving to a [SchemaRevisionResolver].
+ */
 class DefaultAvroSchemaRevisionForSchemaIdResolver(
   private val schemaResolver: AvroSchemaResolver,
   private val schemaRevisionResolver: SchemaRevisionResolver
 ) : AvroSchemaRevisionForSchemaIdResolver {
 
+  /**
+   * Creates a new instance using the [DefaultSchemaRevisionResolver].
+   */
   constructor(schemaResolver: AvroSchemaResolver) : this(schemaResolver, AvroAdapterDefault.schemaRevisionResolver)
 
   override fun apply(id: AvroSchemaId): Optional<AvroSchemaRevision> = schemaResolver.apply(id)
